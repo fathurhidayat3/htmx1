e3ca4e6161c2f492adfb2882df7384dac4a92376
fathurhidayat <fathur.hidayat@amartha.com>
2024-04-22 01:22:25 +0700

    feat: add migration and init readme

---
+++ migrations/1713723220499_todos.js
@@ -0,0 +1,29 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable("htmx1_todos", {
+    id: "uuid",
+    name: { type: "varchar(1000)", notNull: true },
+    is_done: { type: "boolean", default: false },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {};
--- src/configs/database.js
+++ src/configs/database.js
@@ -1,19 +1,7 @@
 const pg = require("pg");
 
-const user = process.env.DB_USER;
-const password = process.env.DB_PASSWORD;
-const host = process.env.DB_HOST;
-const port = process.env.DB_PORT || 5432;
-const database = process.env.DB_NAME;
-
 const db = async () => {
-  const client = new pg.Client({
-    user,
-    password,
-    host,
-    port,
-    database,
-  });
+  const client = new pg.Client(process.env.DATABASE_URL);
 
   await client.connect();
 
