e6c8eaa8aade030cdaffc88a2e8a14c565c9d075
fathurhidayat <fathur.hidayat@amartha.com>
2024-04-21 22:45:52 +0700

    feat: add create todo

--- src/controllers/toDoListController.js
+++ src/controllers/toDoListController.js
@@ -1,19 +1,44 @@
+const { v4: uuidv4 } = require("uuid");
+
 const db = require("../configs/database");
 
 const todoListController = {
   index: async (_, res) => {
-    const raw = (await db()).query("SELECT * from htmx1_todos");
+    const raw = (await db()).query("SELECT * from htmx1_todos ORDER BY name");
 
     try {
       const todoList = (await raw).rows;
 
       (await db()).end();
 
-      res.render("todos/toDoList", { todoList });
+      res.render("pages/toDoPage", { todoList });
     } catch (error) {
       (await db()).end();
 
-      res.render("todos/toDoList", { todoList: [] });
+      res.render("pages/toDoPage", { todoList: [] });
+    }
+  },
+  create: async (req, res) => {
+    const name = req.body.name;
+
+    (await db()).query(
+      "INSERT INTO htmx1_todos (id, name, is_done) VALUES ($1, $2, false) RETURNING *",
+      [uuidv4(), name]
+    );
+
+    // NOTE: better to be handled with refetch/append new item on client
+    const raw = (await db()).query("SELECT * from htmx1_todos ORDER BY name");
+
+    try {
+      const todoList = (await raw).rows;
+
+      (await db()).end();
+
+      res.render("components/todos/toDoList", { todoList });
+    } catch (error) {
+      (await db()).end();
+
+      res.render("components/todos/toDoList", { todoList: [] });
     }
   },
   update: async (req, res) => {
@@ -30,7 +55,7 @@ const todoListController = {
 
       (await db()).end();
 
-      res.render("todos/toDoInput", { todo });
+      res.render("components/todos/toDoCheckbox", { todo });
     } catch (error) {
       (await db()).end();
     }
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const todoListController = require("../controllers/toDoListController");
 const router = express.Router();
 
 router.get("/", todoListController.index);
+router.post("/todos", todoListController.create);
 router.put("/todos/:id", todoListController.update);
 
 module.exports = router;
